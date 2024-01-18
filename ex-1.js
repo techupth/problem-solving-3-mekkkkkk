5c922abe1b566891fe614fdb8039a29128ffe3e5
Parunyu Pitragool <Mek@Meks-MacBook-Pro.local>
2024-01-18 16:45:35 +0700

    feat: add complete ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,13 +1,29 @@
 // Exercise 1: Length of Last Word
 
 const lengthOfLastWord = function (s) {
-  //Start coding here
+  let countWord = "";
+  console.log(s.length);
+  console.log(s);
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    console.log(s[i]);
+    console.log(countWord)
+
+    if (s[i] !== " ") {
+      countWord += s[i];
+    } else if(countWord !== ""){
+      break
+    }
+  }
+  return countWord.length;
 };
 
-const result1 = lengthOfLastWord("Hello World");
-const result2 = lengthOfLastWord("   fly me   to   the moon  ");
-const result3 = lengthOfLastWord("luffy is still joyboy");
+  const result1 = lengthOfLastWord("Hello World");
+  const result2 = lengthOfLastWord("   fly me   to   the moon  ");
+  const result3 = lengthOfLastWord("luffy is still joyboy");
 
-console.log(result1); // 5
-console.log(result2); // 4
-console.log(result3); // 6
+  console.log(result1); // 5
+  console.log(result2); // 4
+  console.log(result3); // 6
+
+
